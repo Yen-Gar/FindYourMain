39c2c32baf9e49eda697b8745ea071831e017de8
y.garcia <y.garcia.bonilla@outlook.com>
2026-05-11 04:50:14 +0200

    stats hotfix unsolved

---
+++ Migrations/20260511024524_UpdateUserIdToString.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FindYourMain.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserIdToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserID",
+                table: "Stats",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UserID",
+                table: "Stats",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
